f341d345ad2e74f567e122449814df6fb9761477
Aditya <techincaladitya86@gmail.com>
2024-10-03 21:24:39 +0530

    i done it

--- models/GroupMentorMappedModel.js
+++ models/GroupMentorMappedModel.js
@@ -9,12 +9,12 @@ const groupMentorSchema = new mongoose.Schema({
   teacherId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "teacherDetail",
-    required: [true, "teacher id is required"],
+    required: [true, "Teacher id is required"],
   },
-  project_id: {
+  projectId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "ProjectDetail",
-    required: [true, "project id is required"],
+    required: [true, "Project id is required"],
   },
 });
 
