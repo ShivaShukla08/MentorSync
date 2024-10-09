38d07441c2c4e0c5fc091d1df7f0f10f13f82e14
Shiva Shukla <112312101+ShivaShukla08@users.noreply.github.com>
2024-10-09 19:04:38 +0530

    Update RequestTableGroupAndStudentModel.js (#8)
    * Update RequestTableGroupAndStudentModel.js
    
    * Update RequestTableGroupAndStudentModel.js

--- models/RequestTableGroupAndStudentModel.js
+++ models/RequestTableGroupAndStudentModel.js
@@ -8,13 +8,13 @@ const requestTableSchema = new mongoose.Schema({
     required: true,
   },
 
-  student_id: {
+  studentId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "studentdetails",
     required: true,
   },
 
-  group_id: {
+  groupId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "presentationgroup",
     required: true,
@@ -25,7 +25,7 @@ const requestTableSchema = new mongoose.Schema({
     date: Date.now(),
   },
 
-  Rollback: {
+  rollback: {
     type: Boolean,
     default: false,
   },
